ce6219da92c92614aaf2ad98d0c8cbe8a9c02809
dron0610 <dronagnihotri@gmail.com>
2024-11-05 23:46:19 +0530

    Initial commit

---
+++ MERN_STACK_GYM_WEBSTITE_WITH_EMAIL_FUNCTIONALITY-main/frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Dron,Brahmjot and Satkirat
+    </footer>
+  )
+}
+
+export default Footer
